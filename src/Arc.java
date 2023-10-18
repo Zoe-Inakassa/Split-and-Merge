e8fe7a6b39fcc4e693f40bdc9c984da2479db1b2
Zoé <zoe.coudray@etu.univ-tours.fr>
2023-10-18 15:39:07 +0200

    adding Arc class

---
+++ src/Arc.java
@@ -0,0 +1,17 @@
+public class Arc {
+    private int start;
+    private int end;
+
+    public Arc(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+}
