6c4493eea17cf0bfd84f2267efd689fa7a255f33
Zoé <zoe.coudray@etu.univ-tours.fr>
2023-10-18 15:04:00 +0200

    reading and writing an image in main method

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,56 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+
+import org.itk.simple.*;
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Entrée with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Maj+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Maj+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        if(args.length<4){
+            System.exit(1);
         }
+
+        ImageFileReader reader = new ImageFileReader();
+        reader.setImageIO("NiftiImageIO");
+        reader.setFileName(args[0]);
+        Image image = reader.execute();
+        if(image.getNumberOfComponentsPerPixel()!=1) System.out.println("carefull number of component per pixel !=1");
+        if(image.getSize().size()!=3){
+            System.exit(2);//bad image dimension
+        }
+        int xImage =Math.toIntExact(image.getSize().get(0));
+        int yImage =Math.toIntExact(image.getSize().get(1));
+        int zImage =Math.toIntExact(image.getSize().get(2));
+
+        //reading picture
+        float[][][] tabImage= new float[xImage][yImage][zImage];
+        VectorUInt32 vector = new VectorUInt32(3,0);
+        Long e = 1L;
+        for(int x=0;x<xImage;x++){
+            vector.set(0,x*e);
+            for(int y=0;y<yImage;y++){
+                vector.set(1,y*e);
+                for(int z=0;z<zImage;z++){
+                    vector.set(2,z*e);
+                    tabImage[x][y][z]= image.getPixelAsFloat(vector);
+                }
+            }
+        }
+
+        //writing picture
+        for(int x=0;x<xImage;x++){
+            vector.set(0,x*e);
+            for(int y=0;y<yImage;y++){
+                vector.set(1,y*e);
+                for(int z=0;z<zImage;z++){
+                    vector.set(2,z*e);
+                    image.setPixelAsFloat(vector,tabImage[x][y][z]);
+                }
+            }
+        }
+        ImageFileWriter writer = new ImageFileWriter();
+        writer.setFileName(args[3]);
+        writer.execute(image);
+
+        System.out.println("fin");
+        System.exit(0);
     }
 }
